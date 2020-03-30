51120b80aa435c98a0ec346c62d99eafed1bc799
ShadedThoughts <62673231+ShadedThoughts@users.noreply.github.com>
2020-03-30 01:50:43 +0100

    new line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 	// Prints "Hello, Java" in the terminal window.
+	// This meant to simulate for POC jenkins trigger
 	System.out.println("Hello, World");
 	
 	}
